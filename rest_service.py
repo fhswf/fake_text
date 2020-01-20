ab8bea7a78c74927fdd7a3d484d134b2691848cd
Christian Gawron <gawron.christian@fh-swf.de>
2020-01-20 21:50:17 +0100

    cleanup

--- rest_service.py
+++ rest_service.py
@@ -1,38 +1,25 @@
 import io
 import ssl
-import urllib.request
 from faker import Faker
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse, request
+from flask import Flask
+from flask_restful import Api, Resource, request
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='/ui')
+app = Flask(__name__, static_url_path='/', static_folder='frontend')
 CORS(app)
 api = Api(app)
 
 
-@app.route('/<path:path>')
-def send_static(path):
-    """Serve static content.
-    Static resources for the demo interface are served via the frontend directory.
-
-    Parameters
-    ----------
-    path : str
-        Path of requested rersource.
-    """
-    return send_from_directory('frontend', path)
-
-
 class FakeText(Resource):
-    """REST end point for the fake text service.
-    """
+    """REST end point for the fake text service."""
 
     def __init__(self):
         self.faker = Faker()
 
     def post(self):
-        """
+        """Create fake text.
+
+        Create four different continuations for text. 
         """
 
         json = request.get_json()
@@ -41,7 +28,7 @@ class FakeText(Resource):
         answer = self.faker.generate_text(text)
 
         data = {"result": answer, "input": json}
-        return data, 201
+        return data, 200
 
 
 api.add_resource(FakeText, "/text")
